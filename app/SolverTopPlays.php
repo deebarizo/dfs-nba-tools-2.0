12506abf6cb2e6472bb90be1a015d7bdcd38f172
Dee Barizo <deebarizo@gmail.com>
2014-12-27 01:01:51 +0000

    Add more lineups to solver top plays

--- app/SolverTopPlays.php
+++ app/SolverTopPlays.php
@@ -192,7 +192,7 @@ class SolverTopPlays {
 
 		$duplicateLineups = [];
 
-		for ($i=0; $i < 250; $i++) { 
+		for ($i=0; $i < 1000; $i++) { 
 			$duplicateLineups[] = $this->buildOneLineupWithTopPlays($players, $numOfPlayersPerPosition);
 		}
 
