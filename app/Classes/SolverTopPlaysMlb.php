08d27b632546369d3c32582e93debbf4f4f1def7
Dee Barizo <deebarizo@gmail.com>
2015-03-18 23:37:41 +0000

    Work on solver top plays MLB

--- app/Classes/SolverTopPlaysMlb.php
+++ app/Classes/SolverTopPlaysMlb.php
@@ -30,15 +30,23 @@ class SolverTopPlaysMlb {
 	public function generateLineups($timePeriodInUrl, $date) {
 		$timePeriod = urlToUcFirst($timePeriodInUrl);
 
-		$topPlays = $this->getTopPlays($timePeriod, $date);
+		$players = $this->getPlayers($timePeriod, $date);
 
-		$positions = $this->getPositions();
+		$positions = $this->getPositions($timePeriod, $date);
 
-		ddAll($topPlays);
+		$lineup = $this->generateLineup($players, $positions);
+
+		# ddAll($players);
+	}	
+
+	private function generateLineup($players, $positions) {
+		$positionKey = rand(0, 6);
+
+
 	}
 
-	private function getPositions() {
-		return [
+	private function getPositions($timePeriod, $date) {
+		$positions = [
 			['name' => 'SP', 'remaining_spots' => 2],
 			['name' => 'C', 'remaining_spots' => 1],
 			['name' => '1B', 'remaining_spots' => 1],
@@ -47,17 +55,74 @@ class SolverTopPlaysMlb {
 			['name' => 'SS', 'remaining_spots' => 1],
 			['name' => 'OF', 'remaining_spots' => 3]
 		];
+
+		$numOfPlayersInPositions = DB::table('player_pools')
+										->select(DB::raw('position as name, count(*) as number'))
+										->join('dk_mlb_players', 'dk_mlb_players.player_pool_id', '=', 'player_pools.id')
+										->where('time_period', $timePeriod)
+										->where('date', $date)
+										->where('target_percentage', '>', 0)
+										->groupBy('position')
+										->get();
+
+		$slashPositions = [];
+
+		foreach ($numOfPlayersInPositions as $key => $numOfPlayersInPosition) {
+			if (preg_match('/\//', $numOfPlayersInPosition->name)) {
+				$firstSlashPosition = preg_replace('/(\w+)(\/)(\w+)/', '$1', $numOfPlayersInPosition->name);
+				$secondSlashPosition = preg_replace('/(\w+)(\/)(\w+)/', '$3', $numOfPlayersInPosition->name);
+
+				$slashPosition = new \stdClass();
+				$slashPosition->name = $firstSlashPosition;
+				$slashPosition->number = $numOfPlayersInPosition->number;
+				$slashPositions[] = $slashPosition;
+
+				$slashPosition = new \stdClass();
+				$slashPosition->name = $secondSlashPosition;
+				$slashPosition->number = $numOfPlayersInPosition->number;
+				$slashPositions[] = $slashPosition;
+
+				unset($numOfPlayersInPositions[$key]);
+			}
+		}
+
+		foreach ($numOfPlayersInPositions as &$numOfPlayersInPosition) {
+			$numOfPlayersInPosition->number += $this->addSlashPosition($numOfPlayersInPosition, $slashPositions);
+		} 
+		unset($numOfPlayersInPosition);
+
+		prf($slashPositions);
+		prf($numOfPlayersInPositions);
 	}
 
-	private function getTopPlays($timePeriod, $date) {
-		return DB::table('player_pools')
-				 ->select('buy_in', 'mlb_player_id', 'target_percentage', 'mlb_team_id', 'position', 'salary', 'name')
-				 ->join('dk_mlb_players', 'dk_mlb_players.player_pool_id', '=', 'player_pools.id')
-				 ->join('mlb_players', 'mlb_players.id', '=', 'dk_mlb_players.mlb_player_id')
-				 ->where('time_period', $timePeriod)
-				 ->where('date', $date)
-				 ->where('target_percentage', '>', 0)
-				 ->get();
+	private function addSlashPosition($numOfPlayersInPosition, $slashPositions) {
+		$number = 0;
+
+		foreach ($slashPositions as $slashPosition) {
+			if ($numOfPlayersInPosition->name == $slashPosition->name) {
+				$number += $slashPosition->number;
+			}
+		}
+
+		return $number;
+	}
+
+	private function getPlayers($timePeriod, $date) {
+		$players = DB::table('player_pools')
+						->select('buy_in', 'mlb_player_id', 'target_percentage', 'mlb_team_id', 'position', 'salary', 'name')
+						->join('dk_mlb_players', 'dk_mlb_players.player_pool_id', '=', 'player_pools.id')
+						->join('mlb_players', 'mlb_players.id', '=', 'dk_mlb_players.mlb_player_id')
+						->where('time_period', $timePeriod)
+						->where('date', $date)
+						->where('target_percentage', '>', 0)
+						->get();
+
+		foreach ($players as &$player) {
+			$player->in_lineup = 0;
+		} 
+		unset($player);
+
+		return $players;
 	}
 
 }
