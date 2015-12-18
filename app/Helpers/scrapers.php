7eb9d185094579b33392090c38edbf6c6baa8800
Dee Barizo <deebarizo@gmail.com>
2015-12-18 18:23:45 +0000

    Misc

--- app/Helpers/scrapers.php
+++ app/Helpers/scrapers.php
@@ -99,7 +99,7 @@ function scrapeForGamesTable($client, $crawler, $tableIDinBR, $teams, $seasonId,
 	$startingGame = $gamesCount + 1;
 	$i = $startingGame;
 
-	$rowCount += 2; // number = number of month rows on basketball reference
+	$rowCount += 3; // number = number of month rows on basketball reference
 
 	do {
 		$gameRow = $crawler->filter('table#'.$tableIDinBR.' > tbody > tr:nth-child('.$i.') > td')->count(); // as opposed to month row
